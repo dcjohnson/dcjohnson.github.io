2ebe8a888852f986340b98f21f689c6b183dd4f6
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2015-03-08 16:17:49 -0400

    Landing page and javascript.

--- js/page-nav.js
+++ js/page-nav.js
@@ -1,5 +1,19 @@
-function ajax() {
+function ajaxGet(onRequestFunc, requestData, isAsync) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			onRequestFunc(xmlHttp.responseText);
+		} else if(xmlHttp.status == 404) {
+			onRequesetFunc("Page not found.");
+		}
+	}
+	xmlHttp.open("GET", requestData, isAsync);
+	xmlHttp.send();
 }
 
 function loadProjects() {
+	var requestFunc = function(responseText) {
+		document.getElementById("content").innerHTML = responseText;
+	}
+	ajaxGet(requestFunc, "../pages/projects.html", true);
 }
